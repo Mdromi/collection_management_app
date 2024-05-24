e102994fa32ca0d875802278f121e544cde04b0b
mdromi <mdromi14@gmail.com>
2024-05-24 23:52:52 +0600

    debug image upload for production - 3

--- app/lib/image_uploadable.rb
+++ app/lib/image_uploadable.rb
@@ -1,14 +1,16 @@
 # app/lib/image_uploadable.rb
 
-require 'net/http'
-require 'uri'
-require 'json'
+require "net/http"
+require "uri"
+require "json"
 
 module ImageUploadable
   def upload_image_to_imgbb(image_data)
     url = URI.parse("https://api.imgbb.com/1/upload")
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
+
+    puts "Env #{ENV["IMGBB_API_KEY"]}"
 
     request = Net::HTTP::Post.new(url)
     request.set_form_data({
@@ -17,7 +19,9 @@ module ImageUploadable
     })
 
     response = http.request(request)
-    puts "response #{response}"
-    JSON.parse(response.body)
+    puts "Response body: #{response.body}"
+    parsed_response = JSON.parse(response.body)
+    puts "Parsed response: #{parsed_response}"
+    parsed_response # return the parsed response
   end
 end
