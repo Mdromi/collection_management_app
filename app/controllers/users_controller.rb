a39749c1afcb73e01b2596263bcb60a2a3fc6cb8
mdromi <mdromi14@gmail.com>
2024-05-30 18:57:54 +0600

    Design the home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
   def index
+    @collections = Collection.includes(:user).order(items_count: :desc).limit(10)
   end
-
+  
   def show
     @user = User.find(params[:id])
     @collections = @user.collections
