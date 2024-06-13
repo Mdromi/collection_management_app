a3243a3e38acdef195b2998148e3478156f5405c
Mdromi <sadrahman.dev@gmail.com>
2024-06-13 14:37:04 +0600

    jira signup

--- app/javascript/controllers/sign_up_controller.js
+++ app/javascript/controllers/sign_up_controller.js
@@ -1,15 +1,23 @@
-// app/javascript/controllers/signUpController.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
     // Fetch sign-up URL from data attribute
-    console.log("Connected");
     const signUpUrl = this.data.get("url");
-    console.log("signUpUrl", signUpUrl);
 
     if (signUpUrl) {
-      window.open(signUpUrl, '_blank');
+      // Open the sign-up URL in a new tab
+      this.openSignUpUrl(signUpUrl);
+    }
+  }
+
+  openSignUpUrl(url) {
+    // Open URL in a new tab
+    const newTab = window.open(url, '_blank');
+    
+    // Check if newTab is null (pop-up blocked or other issue)
+    if (!newTab) {
+      console.error('Failed to open sign-up URL in a new tab');
     }
   }
 }
